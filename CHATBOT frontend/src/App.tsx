2f9407a9fcae849eb6ee0d8a9acd80f95171375e
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-02-15 16:08:36 +0530

    Chatbot - backend (gemini, ts)

---
+++ CHATBOT frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Chatbot from "./components/Chatbot";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Chatbot />
+        </div>
+    );
+};
+
+export default App;
