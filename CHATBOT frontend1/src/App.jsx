8ba7c1c7cc864e3b4fd068d94a327f1308bd619a
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-02-15 16:05:01 +0530

    Chatbot - frontend (gemini)

---
+++ CHATBOT frontend1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chatbot from "./components/Chatbot";
+import "./styles/Chatbot.css"; // Import chatbot styles
+
+function App() {
+  return (
+    <div>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
