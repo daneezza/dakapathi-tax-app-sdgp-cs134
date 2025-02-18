f32424307c4ea8a786914b66f5777953471da9d5
Daneesha Hansaka <daneesha.2003@hotmail.com>
2025-02-18 11:54:38 +0530

    Implemented Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
-  return(
-      <>
+import Template from './components/template';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AboutUs from './pages/AboutUs';
+import Dashboard from './pages/Dashboard';
 
-      </>
+function App() {
+  return (
+    <Router>
+      <Template>
+        <Switch>
+          <Route path="/about" component={AboutUs} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </Template>
+    </Router>
   );
 }
-// hello
-export default App
+
+export default App;
