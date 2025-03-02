fd0fff37767a2df96fca41831ce86c5b8dd208fc
ItsInam <inamirshad2005@gmail.com>
2025-03-02 15:59:20 +0530

    Integrated the template to the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Template from './components/template';
+import './styles/template.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Template>
+        <Routes>
+          
+        </Routes>
+      </Template>
+    </Router>
+  );
+}
+
+export default App;
