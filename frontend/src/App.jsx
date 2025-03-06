cbaf3784794d4e54303777269b7ba7d056a7b849
Daneesha Hansaka <daneesha.2003@hotmail.com>
2025-03-06 22:41:30 +0530

    Implemented main components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
-  return(
-      <>
+import Template from './components/template';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AboutUs from './pages/AboutUs';
+import Dashboard from './pages/Dashboard';
+import TaxCalculator from './pages/TaxCalculator';
+import Settings from './pages/Settings';
 
-      </>
+function App() {
+  return (
+    <Router>
+      <Template>
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/about" component={AboutUs} />
+          <Route path="/taxcalculator" component={TaxCalculator} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/" component={Dashboard} />
+
+        </Switch>
+      </Template>
+    </Router>
   );
 }
-// hello
-export default App
+
+export default App;
