60e23598d2e87c71cad92079f2b35607906cf187
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-03-11 11:16:06 +0530

    Changed folder structure

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import "../styles.css";
+import "../styles/Chatbot.css";
 
 const Chatbot: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
