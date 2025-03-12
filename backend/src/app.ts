7f870e05f53e0677ff870caf512a8bdd73d578fc
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-03-13 00:22:05 +0530

    added tax cal

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,42 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes';
+import otpRoutes from './routes/otpRoutes';
+import forgotPasswordRoutes from './routes/forgotPasswordRoutes';
+import cors from 'cors';
+import questionRoutes from './routes/questionRoutes';
+import { getUserGuides, getUserGuideById } from './controllers/authController';
+import path from 'path';
+import newsRouter from './routes/news';
 import taxRoutes from "./routes/tax.routes";
 
+
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use('/videos', express.static(path.join(__dirname, '../public/videos')));
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/otp', otpRoutes);
+app.use('/api/password', forgotPasswordRoutes);
+app.use('/api/questions', questionRoutes);
+app.use('/api/news', newsRouter);
+
 app.use(express.json());
 app.use("/api/tax", taxRoutes);
 
+// routers for user guide
+app.get('/api/guides', getUserGuides);
+app.get('/api/guides/:id', getUserGuideById);
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
