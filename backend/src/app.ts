66a2f3466cacb3775b46a089842db22f73f6a62e
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-03-12 23:42:34 +0530

    port changed on both back and frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import taxRoutes from "./routes/tax.routes";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TaxCalculator from "./components/TaxCalculator";
-import "./styles/App.css";
+import "./styles/TaxCalculator.css";
 import Template from './components/template';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -8,14 +8,7 @@ const App = () => {
   return (
     <Router>
       <Template>
-        <div className="cal-container">
-          <div className="outer-wrapper"> {/* New Container */}
-            <h1 className="cal-topic">Calculate Your Taxes with Ease</h1>
-            <div className="inner-wrapper"> {/* Container for styling */}
-              <TaxCalculator />
-            </div>
-          </div>
-        </div>
+        <TaxCalculator />
       </Template>
     </Router>
   );
