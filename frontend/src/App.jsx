55521c740a4f26bca5e8acb3a1497d740a229bd6
Daneesha Hansaka <daneesha.2003@hotmail.com>
2025-03-19 15:31:48 +0530

    Extension handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import AboutUs from './pages/AboutUs.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import QnA from './pages/qna.jsx';
 import LearningHub from './pages/LearningHub.jsx';
-import UserGuides from './pages/UserGuides.tsx';
+import UserGuides from './pages/UserGuides.jsx';
 import NewsFeed from './pages/NewsFeed.tsx';
 import NewsDetail from './components/NewsDetail.tsx';
 import Game from './pages/game.tsx';
