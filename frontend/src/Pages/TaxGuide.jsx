9fd3db31f4950fc3607444e5461accc77c12fb91
Stephanperera04 <153462519+Stephanperera04@users.noreply.github.com>
2025-03-21 00:39:41 +0530

    Updated TaxGuide.jsx

--- frontend/src/Pages/TaxGuide.jsx
+++ frontend/src/Pages/TaxGuide.jsx
@@ -17,45 +17,38 @@ const TaxGuides = () => {
       <div className="guides-container">
         {guides.map((guide, index) => {
           const isEven = index % 2 === 0;
+  
+          const videoId = guide.YoutubePath.split('youtu.be/')[1]?.split('?')[0];
+  
           return (
             <React.Fragment key={guide.id}>
-              <div className="guide">
-                {isEven ? (
-                  <>
-                    <div className="video-container">
-                      
-                    </div>
-                    <div className="text-container">
-                      <h2 className="guide-title">
-                        {guide.id}) {guide.title}
-                      </h2>
-                      <div className="guide-content">
-                        {guide.content.map((step, idx) => (
-                          <p key={idx}>{step}</p>
-                        ))}
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className="text-container">
-                      <h2 className="guide-title">
-                        {guide.id}) {guide.title}
-                      </h2>
-                      <div className="guide-content">
-                        {guide.content.map((step, idx) => (
-                          <p key={idx}>{step}</p>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="video-container">
-                     
-                    </div>
-                  </>
-                )}
+              <div className={`guide ${isEven ? "even" : "odd"}`}>
+                <div className="video-container">
+                  {videoId && (
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${videoId}`}
+                      title={`Guide ${guide.id} Video`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    />
+                  )}
+                </div>
+                <div className="text-container">
+                  <h2 className="guide-title">
+                    {guide.id}) {guide.title}
+                  </h2>
+                  <div className="guide-content">
+                    {guide.content.map((step, idx) => (
+                      <p key={idx}>{step}</p>
+                    ))}
+                  </div>
+                </div>
               </div>
-              <br></br>
-              <br></br>
+              <br />
+              <br />
             </React.Fragment>
           );
         })}
