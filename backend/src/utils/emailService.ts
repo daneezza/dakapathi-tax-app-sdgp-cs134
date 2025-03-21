59c640825d5a2c29640fbc554b378559d6454dee
Methuni Mallikaratchy <methuni.20230435@iit.ac.lk>
2025-03-22 00:18:38 +0530

    added comments - emailService, passwordUtil

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -3,21 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Configures the nodemailer transporter for sending emails
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'gmail', // EMail service
     auth: {
-        user: process.env.EMAIL_USER, // Your email
+        user: process.env.EMAIL_USER, // Senders email (Dakapathi official email)
         pass: process.env.EMAIL_PASS, // Your app password
     },
 });
 
+// Send OTP email for verification
 export const sendOTPEmail = async (type:String,subject:string,email: string, otp: string) => {
     const mailOptions = {
         from: process.env.EMAIL_USER,
-        to: email,
+        to: email, // Recivers email
         subject: subject,
         text: `Your ${type} OTP code is: ${otp}. This code is valid for 5 minutes.`,
 };
 
-await transporter.sendMail(mailOptions);
+await transporter.sendMail(mailOptions); // Sends the mail
 };
