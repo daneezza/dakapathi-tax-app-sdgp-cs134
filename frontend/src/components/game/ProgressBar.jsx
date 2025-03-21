5a66bd3c7f6135353a7562d1dd085e6f46076918
Methuni Mallikaratchy <methuni.20230435@iit.ac.lk>
2025-03-22 00:50:03 +0530

    added comments to the game page and components

--- frontend/src/components/game/ProgressBar.jsx
+++ frontend/src/components/game/ProgressBar.jsx
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 const ProgressBar = ({ progress }) => {
   return (
     <div className="progressBar">
+      {/* fills based on progress percentage */}
       <div className="filled" style={{ width: `${progress * 100}%` }} />
     </div>
   )
 }
 
 ProgressBar.propTypes = {
-  progress: PropTypes.number.isRequired
+  progress: PropTypes.number.isRequired //progress must be a number
 }
   
 
