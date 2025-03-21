8d3517aec13e5dc561007265ab7d20f73f83987b
Methuni Mallikaratchy <methuni.20230435@iit.ac.lk>
2025-03-21 23:25:41 +0530

    added comments to valideinput and deleted the empty file

--- backend/src/middlewares/validateInput.ts
+++ backend/src/middlewares/validateInput.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
+// Validate the input fields
 export const validateInput = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
-
+  // Check if mail and password are provided
   if (!email || !password) {
     return res.status(400).json({ message: 'Email and password are required.' });
   }
-  next();
+  next(); // Proceed to the next middleware or route handler
 };
